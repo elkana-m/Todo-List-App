ade1d5b93b915113627bbbe46c54c575ac4cf5aa
elkana-m <munganga.elkana@yahoo.fr>
2022-10-30 23:53:16 -0500

    added title section

--- App.js
+++ App.js
@@ -4,8 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/*Today's Task*/}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+        </View>
+
+      </View>
+      
+
     </View>
   );
 }
@@ -13,8 +23,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    padding: 90,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {},
 });
