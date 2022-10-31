8b713d8dce22a18727c3a89213c8d4657d4be7d3
elkana-m <munganga.elkana@yahoo.fr>
2022-10-31 00:23:19 -0500

    added tasks components styling

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
@@ -11,7 +12,10 @@ export default function App() {
 
         <View style={styles.items}>
           {/* This is where the tasks will go! */}
-        </View>
+          <Task text={'Task 1'}/>
+          <Task text={'Task 2'}/>
+          </View>
+
 
       </View>
       
@@ -33,5 +37,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
-  items: {},
+  items: {
+    marginTop: 30
+  },
 });
