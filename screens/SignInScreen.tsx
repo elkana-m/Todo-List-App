455e97dfac38e205e8e28f284de5e1665cab96e8
elkana-m <munganga.elkana@yahoo.fr>
2022-11-13 23:40:20 -0600

    created sign-in screen

---
+++ screens/SignInScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+const SignInScreen = () => {
+  const [value, setValue] = React.useState({
+    email: '',
+    password: '',
+    error: ''
+  })
+
+  export default SignInScreen;
