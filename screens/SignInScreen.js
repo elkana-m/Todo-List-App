235ebe94f08a852ced5bda38ab1fa470e0df6944
elkana-m <munganga.elkana@yahoo.fr>
2022-11-17 23:30:55 -0600

    designed sign-In UI

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -5,7 +5,21 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function SignIncreen() {
   return (
     <View style={styles.container}>
-      <Text>Sign-In screen!</Text>
+      <Text style={styles.text}>Sign In</Text>
+      <TextInput 
+        style={styles.input} 
+        onChangeText={onChangeText}
+        value={text}
+        placeholder='Email'
+      />
+      <TextInput 
+        style={styles.input} 
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder='Password'
+        keyboardType='numeric'
+        editable
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +28,22 @@ export default function SignIncreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0F1A2E',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: "#fff",
+    fontSize: '30dp',
+    fontStyle: "italic",
+    marginBottom:50
+  },
+  input: {
+    height: 40,
+    width: 320,
+    margin: 12,
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#fff'
   },
 });
