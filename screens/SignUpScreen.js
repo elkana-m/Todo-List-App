b9caffbac74a3ecef2e7d416847503bf5b61a135
elkana-m <munganga.elkana@yahoo.fr>
2022-11-26 00:10:42 -0600

    imported firebase service config and additional modulus we may use

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,3 +1,4 @@
+import { getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
